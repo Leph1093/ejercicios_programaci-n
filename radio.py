3b621900c780cc460c09bf812701ce37d9e1c140
Leph1093 <jorge123valdeon@gmail.com>
2024-09-18 16:08:08 -0500

    Add files via upload

---
+++ radio.py
@@ -0,0 +1,6 @@
+pi= 3.1416
+print ("Calculando el area de un circulo")
+radio= float(input("ingrese el radio del circulo (puede ser entero o decimal): "))
+area_of_the_circle= pi*(radio**2)
+
+print(f"el area del circulo es: {area_of_the_circle}")
